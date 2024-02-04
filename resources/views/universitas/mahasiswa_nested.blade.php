f0439da72e65b6fab567d370334897f8d278b1e7
Reeansa <raihanfebriyansahh@gmail.com>
2024-02-04 16:32:54 +0700

    #

---
+++ resources/views/universitas/mahasiswa_nested.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>belajar laravel</title>
+</head>
+<body>
+    <h2>Data Mahasiswa Menggunakan Nested Array</h2>
+        <ol>
+            <?php foreach ($mahasiswa as $nama) {
+                echo "<li> $nama </li>";
+            } ?>
+        </ol>
+</body>
+</html>
